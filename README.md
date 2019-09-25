aad3cc08e5c8f145a6891bd9ac5e10bb6f106cb6
Max Halford <maxhalford25@gmail.com>
2019-09-25 18:36:39 +0200

    Tidy up

--- README.md
+++ README.md
@@ -1,2 +1,37 @@
-# streaming-cdf-benchmark
-A benchmark to compare algorithms for estimating cumulative density functions (CDF) on streaming data
+# Streaming CDF benchmark
+
+A benchmark to compare algorithms for estimating [cumulative density functions](https://www.wikiwand.com/en/Cumulative_distribution_function) (CDF) on streaming data. This is of particular interest to me because being able to estimate CDF functions from a stream of values is important for online decision trees. Note that [the large body of work devoted to streaming quantiles](https://scholar.google.com/scholar?hl=en&as_sdt=0%2C5&q=streaming+quantiles&btnG=) is relevant, as the CDF is simply the inverse of the quantile function. If there any other methods which you would like me to add to the benchmark, then please send me at email at [maxhalford25@gmail.com](mailto:maxhalford25@gmail.com).
+
+## Methods
+
+- **Histogram**: Custom implementation of [*A Streaming Parallel Decision Tree Algorithm*](http://jmlr.org/papers/volume11/ben-haim10a/ben-haim10a.pdf). I implemented this myself. You can find another implementation written in Golang [here](https://github.com/VividCortex/gohistogram).
+- **KLL**: Implementation of [*Optimal Quantile Approximation in Streams*](https://arxiv.org/abs/1603.05346). I found an implementation [here](https://github.com/edoliberty/streaming-quantiles), to which I added a `cdf(x)` method. Furthermore I made the algorithm deterministic by adding a `seed` parameter.
+- **t-digest**: Implementation of [*Computing Extremely Accurate Quantiles Using t-Digests*](https://arxiv.org/abs/1902.04023). I found an implementation [here](https://github.com/CamDavidsonPilon/tdigest).
+
+Each method has an `update(x)` method as well as a `cdf(x)` method.
+
+## Installation
+
+1. Have Python 3 installed
+2. Clone the repository and navigate to it
+3. Create a [virtual environment](https://docs.python-guide.org/dev/virtualenvs/) (optional)
+4. Run `pip install -r requirements`
+5. Run `python main.py` (run `python main.py -h` to see the possible flags)
+
+## Results
+
+```
+Bimodal gaussian
+
+                 Method           Error (mean)  Error (99th quantile)     Update time (mean)      Query time (mean)
+              Histogram             0.00009279             0.00039842             6μs, 250ns                  227ns
+                    KLL             0.00112768             0.00395346             1μs, 469ns             2μs, 826ns
+               t-digest             0.00006525             0.00030021            38μs, 740ns            21μs, 288ns
+
+Exponential
+
+                 Method           Error (mean)  Error (99th quantile)     Update time (mean)      Query time (mean)
+              Histogram             0.00014409             0.00073424             6μs, 119ns                  120ns
+                    KLL             0.00122809             0.00399595             1μs, 411ns             2μs, 900ns
+               t-digest             0.00005562             0.00021403            38μs, 371ns            22μs, 225ns
+```
--- main.py
+++ main.py
@@ -1,11 +1,15 @@
+import argparse
 import collections
 import random
 import statistics
 import time
 
+import numpy as np
 import tdigest
+import tqdm
 
 import histogram
+from kll import kll
 
 
 def format_ns(d):
@@ -24,6 +28,7 @@ def format_ns(d):
         amount = units[unit]
         quotient, d = divmod(d, amount)
         if quotient > 0:
+            quotient = str(quotient).rstrip('0').rstrip('.')
             parts.append(f'{quotient}{unit}')
         elif d == 0:
             break
@@ -33,57 +38,87 @@ def format_ns(d):
 
 if __name__ == '__main__':
 
-    rng = random.Random(42)
-    n = 100_000
-    m = 100
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-n', help='number of random values to generate', type=int, default=100_000)
+    parser.add_argument('-m', help='number of quantiles to evaluate on', type=int, default=1_000)
+    parser.add_argument('-seed', help='random seed for reproducibility', type=int, default=42)
+    parser.parse_args()
+    args = parser.parse_args()
+
+    rng = random.Random(args.seed)
+    n = args.n
+    m = args.m
     X = [None] * n
 
-    methods = {
-        'Histogram': histogram.Histogram(max_bins=256),
-        'TDigest': tdigest.TDigest()
+    random_funcs = {
+        'Bimodal gaussian': lambda: rng.gauss(5, 1) if rng.random() < .5 else rng.gauss(10, 2),
+        'Exponential': lambda: rng.expovariate(5)
     }
-    errors = collections.defaultdict(list)
-    update_durations = collections.defaultdict(float)
-    query_durations = collections.defaultdict(float)
 
-    for i in range(n):
-        X[i] = rng.gauss(5, 1) if rng.random() < .5 else rng.gauss(10, 2)
-        for name, method in methods.items():
-            tic = time.perf_counter_ns()
-            method.update(X[i])
-            update_durations[name] += time.perf_counter_ns() - tic
+    for func_name, random_func in random_funcs.items():
 
-    X.sort()
+        methods = {
+            'Histogram': histogram.Histogram(max_bins=256),
+            'KLL': kll.KLL(k=256, seed=42),
+            't-digest': tdigest.TDigest(K=256),
+        }
+        errors = collections.defaultdict(list)
+        update_durations = collections.defaultdict(float)
+        query_durations = collections.defaultdict(float)
 
-    for i in range(1, len(X), n // m):
-        x = X[i]
-        q = i / n
+        print('Updating each method...')
+        for i in tqdm.tqdm(range(n)):
+            X[i] = random_func()
+            for name, method in methods.items():
+                tic = time.perf_counter_ns()
+                method.update(X[i])
+                update_durations[name] += time.perf_counter_ns() - tic
 
-        for name, method in methods.items():
-            tic = time.perf_counter_ns()
-            e = method.cdf(x)
-            query_durations[name] += time.perf_counter_ns() - tic
-            errors[name].append(abs(q - e))
+        # Sort all the values in order to get access to the true CDF
+        print('Evaluating CDF approximations...')
+        X.sort()
+        for i in tqdm.tqdm(range(1, len(X), n // m)):
+            x = X[i]
+            q = i / n
 
-    # Make a template to print out the results one method at a time
-    headings = ['Method', 'Mean absolute error', 'Median absolute error', 'Mean update time', 'Mean query time']
-    width = max(map(len, headings)) + 2
-    row_format = '{:>{width}}' * len(headings)
+            for name, method in methods.items():
+                tic = time.perf_counter_ns()
+                e = method.cdf(x)
+                query_durations[name] += time.perf_counter_ns() - tic
+                errors[name].append(abs(q - e))
 
-    # Write down the table headings
-    table = row_format.format(*headings, width=width) + '\n'
+        # Make a template to print out the results one method at a time
+        headings = [
+            'Method',
+            'Error (mean)',
+            'Error (99th quantile)',
+            'Update time (mean)',
+            'Query time (mean)',
+        ]
+        width = max(map(len, headings)) + 2
+        row_format = '{:>{width}}' * len(headings)
 
-    # Write down the true labels row by row
-    table += '\n'.join((
-        row_format.format(
-            name,
-            f'{statistics.mean(errors[name]):.8f}',
-            f'{statistics.median(errors[name]):.8f}',
-            format_ns(update_durations[name] / n),
-            format_ns(query_durations[name] / n),
-            width=width
-        )
-        for name in methods
-    ))
+        # Write down the table headings
+        table = row_format.format(*headings, width=width) + '\n'
 
-    print(table)
+        # Write down the true labels row by row
+        table += '\n'.join((
+            row_format.format(
+                name,
+                # Estimation error
+                f'{statistics.mean(errors[name]):.8f}',
+                f'{np.quantile(errors[name], .99):.8f}',
+                # Update duration
+                format_ns(update_durations[name] / n),
+                # Querying duration
+                format_ns(query_durations[name] / n),
+                width=width
+            )
+            for name in methods
+        ))
+
+        print()
+        print(func_name)
+        print()
+        print(table)
+        print()
